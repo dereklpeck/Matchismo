7c26cca4dcf155ad1d6798975d74fcc8a7a887fa
Derek Peck <derek@ringseven.com>
2014-09-26 22:41:10 -0600

    first commit

---
+++ Matchismo1/CardMatchingGame.swift
@@ -0,0 +1,9 @@
+//
+//  CardMatchingGame.swift
+//  Matchismo1
+//
+//  Created by Derek Peck on 9/18/14.
+//  Copyright (c) 2014 IS 543. All rights reserved.
+//
+
+import Foundation
---
+++ Matchismo1/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  Matchismo1
+//
+//  Created by Derek Peck on 9/11/14.
+//  Copyright (c) 2014 IS 543. All rights reserved.
+//
+
+import Foundation
