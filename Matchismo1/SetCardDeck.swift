0aa7085802aadb44a9ccaae92b3d519ac8d1d4ca
Derek Peck <derek@ringseven.com>
2014-10-02 16:16:31 -0600

    Implemented SetCardDeck

--- Matchismo1/SetCardDeck.swift
+++ Matchismo1/SetCardDeck.swift
@@ -9,5 +9,23 @@
 import Foundation
 
 class SetCardDeck: Deck {
-    
+    override init() {
+        super.init()
+        
+        for shape in SetCard.validShapes() {
+            for color in SetCard.validColors() {
+                for shade in SetCard.validShades() {
+                    for number in 1 ... SetCard.maxNumber() {
+                        var card = SetCard()
+                        
+                        card.number = number
+                        card.shade = shade
+                        card.color = color
+                        card.shape = shape
+                        addCard(card, atTop: true)
+                    }
+                }
+            }
+        }
+    }
 }
